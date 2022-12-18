12aaadeb119c3af826aa7a62a6842abef856e86d
NeilKleistGao <neilkleist@gmail.com>
2022-12-18 18:16:02 +0800

    Add dependencies

---
+++ src/extention.ts
@@ -0,0 +1,13 @@
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+export function activate(context: ExtensionContext) {
+
+}
