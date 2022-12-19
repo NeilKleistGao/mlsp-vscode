0873575c4f2a06f903b0757045ae18da03990476
NeilKleistGao <neilkleist@gmail.com>
2022-12-19 11:53:12 +0800

    Use child process

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -1,16 +1,35 @@
+import { ChildProcess, spawn } from "child_process";
 import * as path from "path";
 import { ExtensionContext } from "vscode";
 
 import {
   LanguageClient,
   LanguageClientOptions,
-  ServerOptions,
-  TransportKind
+  ServerOptions
 } from "vscode-languageclient";
 
 let client: LanguageClient;
 
 export function activate(p_context: ExtensionContext) {
+  const client_option: LanguageClientOptions = {
+    documentSelector: [{scheme: "file", language: "mlscript"}]
+  };
+
+  const server_option: ServerOptions = () => {
+    return new Promise<ChildProcess>((p_resolve, p_reject) => {
+      try {
+        const child = spawn("java", ["-jar", path.join("server", "mlsp.jar")]);
+        p_resolve(child);
+      }
+      catch {
+        p_reject("can't create mlsp server.");
+      }
+    });
+  };
+
+  client = new LanguageClient(
+    "mlsp", server_option, client_option
+  );
 }
 
 export function diactivate(): Thenable<void> | undefined {
